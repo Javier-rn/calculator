12fd6e04aa36a68d5120313fd184ae4d2fe8364f
Javier <javier96rn@gmail.com>
2022-04-01 17:00:07 +0200

    Restructure logic, now operations can be made

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const allBtns = document.querySelectorAll('.btn');
+const operationBtns = document.querySelectorAll('.operation');
 const clearBtn = document.getElementById('clear');
 const equalBtn = document.getElementById('equal');
-const operationBtns = document.querySelectorAll('.operation');
 const display = document.querySelector('.display');
 
 function add(a, b) {
@@ -28,22 +28,59 @@ let displayValue = '';
 
 let num1 = '';
 let num2 = '';
+let operation;
 
-allBtns.forEach((btn) => {
-    btn.addEventListener('click', function (e) {
-        if (e.target.id === 'add') {
-            displayValue += ' + ';
-        } else if (e.target.id === 'subtract') {
-            displayValue += ' - ';
-        } else if (e.target.id === 'multiply') {
-            displayValue += ' * ';
-        } else if (e.target.id === 'divide') {
-            displayValue += ' / ';
-        } else {
+allBtns.forEach((button) => {
+    button.addEventListener('click', function (e) {
+        if (operation === undefined) {
+            num1 += e.target.id;
             displayValue += e.target.id;
+            display.textContent = displayValue;
+        } else {
+            num2 += e.target.id;
+            displayValue += e.target.id;
+            display.textContent = displayValue;
         }
-        display.textContent = displayValue;
-
-        console.log(displayValue.split(''));
     });
+});
+
+operationBtns.forEach((button) => {
+    button.addEventListener('click', function (e) {
+        if (num1 && num2) {
+            num1 = parseInt(num1);
+            num2 = parseInt(num2);
+            num1 = operate(operation, num1, num2);
+            num2 = '';
+            displayValue = num1;
+            display.textContent = displayValue;
+        }
+        if (e.target.id === '+') {
+            operation = add;
+            displayValue += ' ' + e.target.id + ' ';
+            display.textContent = displayValue;
+        } else if (e.target.id === '-') {
+            operation = subtract;
+            displayValue += ' ' + e.target.id + ' ';
+            display.textContent = displayValue;
+        } else if (e.target.id === '*') {
+            operation = multiply;
+            displayValue += ' ' + e.target.id + ' ';
+            display.textContent = displayValue;
+        } else if (e.target.id === '/') {
+            operation = divide;
+            displayValue += ' ' + e.target.id + ' ';
+            display.textContent = displayValue;
+        }
+    });
+});
+
+equalBtn.addEventListener('click', function () {
+    if (num1 && num2) {
+        num1 = parseInt(num1);
+        num2 = parseInt(num2);
+        num1 = operate(operation, num1, num2);
+        num2 = '';
+        displayValue = num1;
+        display.textContent = displayValue;
+    }
 });
