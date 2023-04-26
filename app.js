4836fee6bfcd5e34c1a2a946cd7f75a105cc2fb2
Javier-rn <javier96rn@gmail.com>
2023-04-26 16:41:08 +0200

    Create evaluation logic

--- app.js
+++ app.js
@@ -27,19 +27,52 @@ function operate(num1, num2, operator) {
   }
 }
 
+function convertOperation(operation) {
+  if (operation === 'add') {
+    return '+';
+  } else if (operation === 'subtract') {
+    return '-';
+  } else if (operation === 'multiply') {
+    return '*';
+  } else if (operation === 'divide') {
+    return '/';
+  } else if (operation === '+') {
+    return 'add';
+  } else if (operation === '-') {
+    return 'subtract';
+  } else if (operation === '*') {
+    return 'multiply';
+  } else if (operation === '/') {
+    return 'divide';
+  }
+}
+
+function evaluateOperation(str) {
+  const splitStr = str.split(' ');
+  const num1 = parseInt(splitStr[0]);
+  const num2 = parseInt(splitStr[2]);
+  const operation = convertOperation(splitStr[1]);
+  if (splitStr.length === 3) {
+    return operate(num1, num2, operation);
+  } else {
+    return false;
+  }
+}
+
 const numBtns = document.querySelectorAll('.num');
 const operationBtns = document.querySelectorAll('.operation');
+const equalBtn = document.querySelector('.equal');
+const clearBtn = document.querySelector('.clear');
 
 numBtns.forEach((num) => {
   num.addEventListener('click', function (e) {
     const calcDisplay = document.querySelector('.calculation');
     const calcText = calcDisplay.textContent;
     const lastChar = calcText.charAt(calcText.length - 1);
-    if (typeof parseInt(lastChar) === 'number') {
-      calcDisplay.textContent += e.target.id;
-    }
-    if (!calcDisplay.textContent) {
+    if (calcDisplay.textContent === '0') {
       calcDisplay.textContent = e.target.id;
+    } else if (typeof parseInt(lastChar) === 'number') {
+      calcDisplay.textContent += e.target.id;
     }
   });
 });
@@ -49,8 +82,14 @@ operationBtns.forEach((btn) => {
     const calcDisplay = document.querySelector('.calculation');
     const calcText = calcDisplay.textContent;
     const lastChar = calcText.charAt(calcText.length - 1);
-    if (typeof lastChar === 'number') {
-      console.log(lastChar);
+    if (!(typeof parseInt(lastChar) === 'number')) {
+      console.log('cant');
     }
+    calcDisplay.textContent += ' ' + convertOperation(e.target.id) + ' ';
   });
+});
+
+equalBtn.addEventListener('click', function (e) {
+  const calcDisplay = document.querySelector('.calculation');
+  calcDisplay.textContent = evaluateOperation(calcDisplay.textContent);
 });
